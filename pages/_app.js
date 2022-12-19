fe7e05f4fbd2d5b93f71273b51365c5a0f9272e1
lojust <logue.justin@gmail.com>
2022-12-19 11:07:08 +0000

    Add styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css';
+import '../styles/global-grid.css';
+import '../styles/spacing.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
